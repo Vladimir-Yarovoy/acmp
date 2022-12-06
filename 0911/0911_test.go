10b366aa74fd8db0f097afc9de954a8b638a0276
vovaenjoy1 <vovaenjoy@icloud.com>
2022-12-06 22:51:19 +0300

    ticket-0911

---
+++ 0911/0911_test.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"testing"
+	// "github.com/stretchr/testify/assert"
+)
+
+func TestAll(t *testing.T) {
+	t.Run("Test 1", func(t *testing.T) {
+		// assert.Equal(t, IsTriangle(3, 4, 5), "YES")
+	})
+
+	t.Run("Test 2", func(t *testing.T) {
+		// assert.Equal(t, IsTriangle(1, 1, 5), "NO")
+	})
+
+	t.Run("Test 3", func(t *testing.T) {
+		// assert.Equal(t, IsTriangle(2, 3, 5), "NO")
+	})
+
+}
